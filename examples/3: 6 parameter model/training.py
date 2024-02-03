4d0b62bd4d58c7387f5f83ad5fd9c07a806cb5f9
Daniel-Lanchares <124088449+Daniel-Lanchares@users.noreply.github.com>
2024-02-03 16:10:16 +0100

    Continued with 6p training.
    Hit the RAM limit for datasets, so train in batches picking randomly from the pool.
    
    Doing an overfitting test to see whether the model can learn the distributions, even if its won't predict them. Promising,yet taking time.

--- examples/3: 6 parameter model/training.py
+++ examples/3: 6 parameter model/training.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from pprint import pprint
+import numpy as np
 
 from torchvision import models
 from dtempest.core import Estimator
@@ -7,11 +9,11 @@ from dtempest.core.common_utils import load_rawsets, seeds2names
 from dtempest.gw.conversion import convert_dataset
 import dtempest.core.flow_utils as trans
 
-files_dir = Path('/home/daniel/Documentos/GitHub/MSc-files')
+files_dir = Path('/media/daniel/easystore/Daniel/MSc-files')
 rawdat_dir = files_dir / 'Raw Datasets'
 trainset_dir = files_dir / 'Trainsets'
-train_dir = files_dir / 'Examples' / '3: 6 parameter model'
-traindir = train_dir / 'training_test_1'
+train_dir = files_dir / 'Examples' / '3. 6 parameter model'
+traindir = train_dir / 'training_test_4'
 
 params_list = [
     'chirp_mass',
@@ -28,16 +30,16 @@ extractor_config = {
     }
 
 train_config = {
-    'num_epochs': 10,
+    'num_epochs': 30,
     'checkpoint_every_x_epochs': 5,  # Not yet implemented
-    'batch_size': 128,
+    'batch_size': 256,  # Almost no effect on RAM compared, but proportional to chance of nan loss at beginning
     'optim_type': 'Adam',  # 'SGD'
-    'learning_rate': 0.0005,
+    'learning_rate': 0.00005,#0.00025,
     'grad_clip': None,
     # 'sched_kwargs': {
     #     'type': 'StepLR',
-    #     'step_size': 15,  # Note epochs go from 0 to num_epochs-1
-    #     'gamma': 0.5,
+    #     'step_size': 5,  # Note epochs go from 0 to num_epochs-1
+    #     'gamma': 0.8,
     #     'verbose': True
     #     }
     }
@@ -45,12 +47,12 @@ train_config = {
 flow_config = {  # This config seems to top at log_prob ~4, tough it may slowly improve
     'input_dim': len(params_list),
     'context_dim': extractor_config['n_features'],
-    'num_flow_steps': 3,  # Adding more seemed to help slightly
+    'num_flow_steps': 5,  # Adding more seemed to help slightly
 
     'base_transform': trans.mask_affine_autoreg,
     'base_transform_kwargs': {
         'hidden_dim': 2,
-        'num_transform_blocks': 2,
+        'num_transform_blocks': 3,
         'use_batch_norm': True
     },
     'middle_transform': trans.random_perm_and_lulinear,
@@ -63,21 +65,95 @@ flow_config = {  # This config seems to top at log_prob ~4, tough it may slowly 
     }
 }
 
-dataset = load_rawsets(rawdat_dir, seeds2names(range(15)))
+rng = np.random.default_rng(4)
+seeds = rng.choice(np.arange(0, 45), size=1, replace=False)
+print(seeds)
+'''
+0: [ 9 20 38 43 10 32 26  1 21 16 44  2  0  6 40]
+1: [11 35 24  5 17  1 32 43 44  9 14 16 36 12 30]
+2: [39 16 24 13 33 14  8 30 29 42 43  3 10 37 25]
+3: [32 18 13 26 28 11  1 22  8  6  2 25 21  3  5]
+4: [36 30 32  3 26 11 43 22 38 17 35 44 15 24 29]
+'''
 
-trainset = convert_dataset(dataset, params_list)
+dataset = load_rawsets(rawdat_dir, seeds2names(seeds))
 
-del dataset
+# Convert to Trainset object (pd.DataFrame based instead of OrderedDict based)
+dataset = convert_dataset(dataset, params_list)
 
 pre_process = models.ResNet18_Weights.DEFAULT.transforms(antialias=True)  # True for internal compatibility reasons
 
-
-# flow = Estimator(params_list, flow_config, extractor_config, name='v3.1.0',
+'''Flow creation'''
+# flow = Estimator(params_list, flow_config, extractor_config, name='v3.4.0',
 #                  workdir=traindir, mode='extractor+flow', preprocess=pre_process)
-flow = Estimator.load_from_file(traindir/'v3.1.0.pt')
+
+'''Training continuation of previous model'''
+flow = Estimator.load_from_file(traindir/'overfitting_test.pt')
+flow.rename('overfitting_test')
+
 # from pprint import pprint
 # pprint(flow.metadata['train_history'])
-flow.rename('v3.1.1')
 
-flow.train(trainset, traindir, train_config)
+
+flow.train(dataset, traindir, train_config)
 flow.save_to_file(traindir/f'{flow.name}.pt')
+
+'''
+3.3.1
+Average: 14.6242, Delta: -0.0119072 (-0.0813549%)
+
+3.3.1 (Ups...)
+Average: 14.6012, Delta: -0.0148934 (-0.101897%)
+
+3.3.2
+Average: ~14.49, Delta: ~-0.010 
+
+3.3.3
+Average: 14.4867, Delta: -0.010512 (-0.072511%)
+
+3.3.4
+Average: 14.382, Delta: -0.00916966 (-0.0637172%)
+
+3.3.5
+Average: 14.4043, Delta: -0.00924209 (-0.0641211%)
+'''
+
+'''
+Deeper architecture
+
+3.4.0
+Average: 28.6662, Delta: -0.855203 (-2.89689%)
+
+3.4.1
+Average: 14.7765, Delta: -0.0769894 (-0.518324%)
+
+3.4.2
+Average: 14.3905, Delta: -0.0195544 (-0.135699%)
+
+3.4.3
+Average: 14.2216, Delta: -0.0153599 (-0.107887%)
+
+test (only dataset 32, overfitting test to know whether the model can learn)
+Average: 13.4706, Delta: -0.0815128 (-0.601477%)
+
+overfitting_test
+Average: 10.5968, Delta: -0.0199011 (-0.18745%)
+
+Average: 10.2693, Delta: -0.020182 (-0.196143%)
+
+Average: 9.88913, Delta: -0.0206944 (-0.208827%)
+
+Average: 9.44537, Delta: -0.00135574 (-0.0143515%)
+
+Average: 9.0541, Delta: -0.0134069 (-0.147857%)
+
+Average: 8.73329, Delta: -0.00181274 (-0.0207524%)
+
+Average: 8.4892, Delta: -0.00779676 (-0.0917591%)
+
+Average: 8.2777, Delta: -0.00833778 (-0.100625%)
+
+Average: 8.065, Delta: -0.00966291 (-0.11967%)
+
+Average: 7.87069, Delta: -0.00107355 (-0.013638%)
+'''
--- examples/3: 6 parameter model/usage.py
+++ examples/3: 6 parameter model/usage.py
@@ -19,26 +19,26 @@ from pesummary.gw.conversions import convert
 
 '''
 
-files_dir = Path('/home/daniel/Documentos/GitHub/MSc-files')
+files_dir = Path('/media/daniel/easystore/Daniel/MSc-files')
 rawdat_dir = files_dir / 'Raw Datasets'
 trainset_dir = files_dir / 'Trainsets'
-train_dir = files_dir / 'Examples' / '3: 6 parameter model'
-traindir0 = train_dir / 'training_test_1'
+train_dir = files_dir / 'Examples' / '3. 6 parameter model'
+traindir0 = train_dir / 'training_test_4'
 catalog_dir = files_dir / 'GWTC-1 Samples'
 
 
-flow0 = Estimator.load_from_file(traindir0 / 'v3.1.1.pt')
+flow0 = Estimator.load_from_file(traindir0 / 'overfitting_test.pt')
 # flow1 = Estimator.load_from_file(traindir4 / 'v0.4.3.pt')
 flow0.eval()
 # flow1.eval()
 
-seed = 11
+seed = 32#999
 event = f'{seed}.00001'
 
 dataset = load_rawsets(rawdat_dir, seeds2names(seed))
 trainset = convert_dataset(dataset, flow0.param_list, name=f'Dataset {seed}')
 
-sset0 = flow0.sample_set(3000, trainset[:][:10], name=flow0.name)
+sset0 = flow0.sample_set(3000, trainset[:][:], name=flow0.name)
 
 error = sset0.accuracy_test(sqrt=True)
 
@@ -63,3 +63,100 @@ print(error.mean(axis=0))
 samples = flow0.sample_and_log_prob(3000, trainset['images'][0])
 print(-torch.mean(samples[1]))
 plt.show()
+
+
+'''
+Dataset 15
+
+|            |   MSE from v3.1.4 | units     |
+|:-----------|------------------:|:----------|
+| chirp_mass |         12.3917   | M_{\odot} |
+| mass_ratio |          0.199914 | ø         |
+| chi_eff    |          0.193486 | ø         |
+| d_L        |        657.537    | Mpc       |
+| ra         |          1.50867  | rad       |
+| dec        |          0.560015 | rad       |
+
+
+|            |   MSE from v3.1.6 | units     |
+|:-----------|------------------:|:----------|
+| chirp_mass |         12.4207   | M_{\odot} |
+| mass_ratio |          0.199407 | ø         |
+| chi_eff    |          0.194147 | ø         |
+| d_L        |        658.376    | Mpc       |
+| ra         |          1.50471  | rad       |
+| dec        |          0.560456 | rad       |
+
+Dataset 30
+|            |   MSE from v3.2.1 | units     |
+|:-----------|------------------:|:----------|
+| chirp_mass |         11.6215   | M_{\odot} |
+| mass_ratio |          0.200057 | ø         |
+| chi_eff    |          0.198596 | ø         |
+| d_L        |        625.488    | Mpc       |
+| ra         |          1.53152  | rad       |
+| dec        |          0.565512 | rad       |
+
+|            |   MSE from v3.2.2 | units     |
+|:-----------|------------------:|:----------|
+| chirp_mass |         11.6114   | M_{\odot} |
+| mass_ratio |          0.200534 | ø         |
+| chi_eff    |          0.19712  | ø         |
+| d_L        |        631.018    | Mpc       |
+| ra         |          1.52971  | rad       |
+| dec        |          0.564508 | rad       |
+
+Dataset 999
+
+|            |   MSE from v3.3.2 | units     |
+|:-----------|------------------:|:----------|
+| chirp_mass |         12.2341   | M_{\odot} |
+| mass_ratio |          0.204443 | ø         |
+| chi_eff    |          0.189867 | ø         |
+| d_L        |        635.43     | Mpc       |
+| ra         |          1.55457  | rad       |
+| dec        |          0.558516 | rad       |
+tensor(14.3822, grad_fn=<NegBackward0>)
+
+|            |   MSE from v3.3.4 | units     |
+|:-----------|------------------:|:----------|
+| chirp_mass |         12.1986   | M_{\odot} |
+| mass_ratio |          0.197778 | ø         |
+| chi_eff    |          0.193448 | ø         |
+| d_L        |        628.704    | Mpc       |
+| ra         |          1.54882  | rad       |
+| dec        |          0.561327 | rad       |
+tensor(14.2551, grad_fn=<NegBackward0>)
+
+|            |   MSE from v3.3.5 | units     |
+|:-----------|------------------:|:----------|
+| chirp_mass |         11.8113   | M_{\odot} |
+| mass_ratio |          0.19896  | ø         |
+| chi_eff    |          0.189573 | ø         |
+| d_L        |        620.747    | Mpc       |
+| ra         |          1.55716  | rad       |
+| dec        |          0.555149 | rad       |
+tensor(14.2631, grad_fn=<NegBackward0>)
+
+|            |   MSE from v3.4.3 | units     |
+|:-----------|------------------:|:----------|
+| chirp_mass |         11.8085   | M_{\odot} |
+| mass_ratio |          0.636145 | ø         |
+| chi_eff    |          0.207094 | ø         |
+| d_L        |        645.777    | Mpc       |
+| ra         |          1.57683  | rad       |
+| dec        |          0.555563 | rad       |
+tensor(14.2310, grad_fn=<NegBackward0>)
+
+
+Dataset 32
+|            |   MSE from overfitting_test | units     |
+|:-----------|----------------------------:|:----------|
+| chirp_mass |                    5.2059   | M_{\odot} |
+| mass_ratio |                    0.122846 | ø         |
+| chi_eff    |                    0.148944 | ø         |
+| d_L        |                  211.735    | Mpc       |
+| ra         |                    0.804376 | rad       |
+| dec        |                    0.437851 | rad       |
+tensor(8.9181, grad_fn=<NegBackward0>)
+'''
